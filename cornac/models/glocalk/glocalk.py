6721e678d9da6da537309170975d7c4f33deda2b
flywithu <34819252+flywithu@users.noreply.github.com>
2023-12-12 22:48:43 +0900

    Update glocalk.py

--- cornac/models/glocalk/glocalk.py
+++ cornac/models/glocalk/glocalk.py
@@ -46,6 +46,7 @@ class GLocalK(nn.Module):
     dot_scale, # dot product weight for global kernel
     pre_min,
     pre_max,
+    use_gpu,
     ):
       self.device = device
       self.n_hid = n_hid
@@ -64,6 +65,7 @@ class GLocalK(nn.Module):
       self.verbose = verbose
       self.pre_min = pre_min
       self.pre_max = pre_max
+      self.use_gpu = use_gpu
 
 
 class KernelLayer(nn.Module):
